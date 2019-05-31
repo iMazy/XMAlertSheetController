9d7dec166c60a6487289d40b9a5de1d07f41e26f
Mazy <mazy_ios@163.com>
2019-05-31 20:22:45 +0800

    feat: init object

---
+++ XMAlertSheetController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XMAlertSheetController
+//
+//  Created by Mazy on 2019/5/31.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
