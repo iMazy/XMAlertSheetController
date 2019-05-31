ebf37e770ec001d98bf93b5f6eb635fc4acbf8f8
Mazy <mazy_ios@163.com>
2019-05-31 20:45:28 +0800

    feat: finish alert action

--- XMAlertSheetController/AlertSheet/XMAlertAction.swift
+++ XMAlertSheetController/AlertSheet/XMAlertAction.swift
@@ -8,14 +8,70 @@
 
 import UIKit
 
-class XMAlertAction: UIButton {
+enum XMAlertActionStyle: Int {
+    
+    case `default`
+    case cancel
+    case destructive
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+open class XMAlertAction: UIButton {
+
+    open var action: (() -> Void)?
+    
+    var actionStyle: XMAlertActionStyle = .cancel
+    
+    lazy var separator = UIView()
+    
+    init() {
+        super.init(frame: .zero)
     }
-    */
+    
+    convenience init(title: String, style: XMAlertActionStyle, action: (() -> Void)? = nil) {
+        self.init()
+        self.action = action
+        self.actionStyle = style
 
+        self.addTarget(self, action: #selector(tappedAction), for: .touchUpInside)
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        
+        switch style {
+        case .default:
+            self.setTitleColor(UIColor.black, for: .normal)
+        case .destructive:
+            self.setTitleColor(UIColor.red, for: .normal)
+        case .cancel:
+            self.setTitleColor(UIColor.darkGray, for: .normal)
+        default:
+            break
+        }
+        self.addSeparatorLine()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension XMAlertAction {
+    
+    @objc func tappedAction() {
+        //Action need to be fired after alert dismiss
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            self?.action?()
+        }
+    }
+    
+    func addSeparatorLine() {
+        separator.backgroundColor = UIColor.lightGray
+        self.addSubview(separator)
+        
+        // Autolayout separator
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        separator.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        separator.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor, constant: -15).isActive = true
+        separator.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor, constant: 15).isActive = true
+        separator.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+    }
 }
--- XMAlertSheetController/ViewController.swift
+++ XMAlertSheetController/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UIAlertAction(title: <#T##String?#>, style: .destructive) { (<#UIAlertAction#>) in
+            <#code#>
+        }
+        
     }
 
 
