db6ad591af109a15a440ecd6d01e032ae70d74f1
Mazy <mazy_ios@163.com>
2019-05-31 20:27:07 +0800

    feat: add base vc and view

---
+++ XMAlertSheetController/AlertSheet/XMAlertAction.swift
@@ -0,0 +1,21 @@
+//
+//  XMAlertAction.swift
+//  XMAlertSheetController
+//
+//  Created by Mazy on 2019/5/31.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class XMAlertAction: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ XMAlertSheetController/AlertSheet/XMAlertSheetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  XMAlertSheetViewController.swift
+//  XMAlertSheetController
+//
+//  Created by Mazy on 2019/5/31.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class XMAlertSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
